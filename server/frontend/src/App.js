ace26896d06bc288537fec325099de00e670b3b8
Shahana-salam <salamshahana393@gmail.com>
2025-07-22 09:49:52 -0400

    changes made from theia lab environment

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -3,6 +3,7 @@ import RegisterPanel from "./components/Register/Register"
 import { Routes, Route } from "react-router-dom";
 import Dealer from "./components/Dealers/Dealer"
 import Dealers from './components/Dealers/Dealers';
+import PostReview from "./components/Dealers/PostReview";
 
 function App() {
   return (
@@ -10,7 +11,9 @@ function App() {
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<RegisterPanel />} />
       <Route path="/dealers" element={<Dealers/>} />
-       <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
+
     </Routes>
   );
 }
